20df50be14e6ce95afd676eff77db4f1549d9210
habib <habib.abdelgaber@yahoo.com>
2021-11-14 17:26:58 +0200

    Python  bootcamp course for everybody class 3

---
+++ arithmetic.py
@@ -0,0 +1,27 @@
+# other data types --> boolean => bool, None => None
+any = None # => None datatype
+print(any)
+print(type(any))
+print(type(None))
+false = False
+print(false)
+print(type(false))
+print(bool(false))
+print(bool(False))
+print('--------------------------')
+true = True
+print(true)
+print(type(true))
+print(bool(true))
+print(bool(True))
+num1 = 1
+string1 = ''
+print(f'this is {bool(num1)}')
+print(f'this is {bool(string1)}')
+num2 = 0
+string2 = 'test'
+print(f'this is {bool(num2)}')
+print(f'this is {bool(string2)}')
+print(f'this is {bool(None)}')
+# Arithmetic operators --> = assignment, + addition,
+# * multiplication, - subtraction, / division with return of float, // division returns floor.
